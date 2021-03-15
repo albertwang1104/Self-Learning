a1b2272f13d8c27dd9748f81a9e6dabe880787db
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-03-15 14:56:20 +0800

    成績指標

---
+++ b964 - 成績指標.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 13 14:43:37 2021
+
+@author: user
+"""
+'''
+10
+0 11 22 33 55 66 77 99 88 44
+
+1
+13
+
+2
+73 65
+
+'''
+a = int(input())
+score = list(map(int, input().split(' ')))
+score.sort()
+
+for i in score:
+    print(i, end = ' ')
+print()    
+
+for i in reversed(score):
+    if i < 60:
+        print(i)
+        break
+    if i == score[0] :
+        print('best case')
+
+for i in score:
+    if i >= 60:
+        print(i)
+        break
+    if i == score[-1] :
+        print('worst case')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
