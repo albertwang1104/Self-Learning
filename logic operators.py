ae6fce3d0fd61706ede5d5a89292d090191906ab
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-03-22 13:31:43 +0800

    Update logic operators.py

--- logic operators.py
+++ logic operators.py
@@ -1,17 +1,15 @@
-def logic(a,b,c):
-    if a > 0:
-        a = 1
-    if b > 0:
-        b = 1
-    
-    if a & b == c:
-        print('AND')
-    if a | b == c:
-        print('OR')
-    if a ^ b == c:
-        print('XOR')
-    if a & b != c and a | b != c and a ^ b != c:
-        print('IMPOSSIBLE')
-
 a, b, c = map(int, input().split(' '))
-logic(a,b,c)
+
+if a > 0:
+    a = 1
+if b > 0:
+    b = 1
+    
+if a & b == c:
+    print('AND')
+if a | b == c:
+    print('OR')
+if a ^ b == c:
+    print('XOR')
+if a & b != c and a | b != c and a ^ b != c:
+    print('IMPOSSIBLE')
