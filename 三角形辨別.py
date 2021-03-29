d221262f7c9aaa20857e2052e8bf3b9fa78b67ff
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-03-29 13:50:40 +0800

    Add files via upload

--- 三角形辨別.py
+++ 三角形辨別.py
@@ -1,18 +1,21 @@
 try:
     triangle = list(map(int, input('').split(' ')))
     triangle.sort()
+    a = triangle[0]
+    b = triangle[1]
+    c = triangle[2]
     
-    if triangle[0] + triangle[1] <= triangle[2]:
-        print(triangle[0], triangle[1], triangle[2])
+    if a + b <= c:
+        print(a, b, c)
         print('No')
-        if triangle[0]**2 + triangle[1]**2 == triangle[2]**2:
-            print(triangle[0], triangle[1], triangle[2])
-            print('Right')
-        elif triangle[0]**2 + triangle[1]**2 < triangle[2]**2:
-            print(triangle[0], triangle[1], triangle[2])
-            print('Acute')
-        else:
-            print(triangle[0], triangle[1], triangle[2])
-            print('Obtuse')
+    elif a**2 + b**2 == c**2:
+        print(a, b, c)
+        print('Right')
+    elif a**2 + b**2 < c**2:
+        print(a, b, c)
+        print('Obtuse')
+    else:
+        print(a, b, c)
+        print('Acute')
 except:
-    pass
+    pass 
