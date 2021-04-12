3cf00e2cf638cd5a1e9f8b873f1e485ef5137927
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-04-12 13:36:36 +0800

    Add files via upload

--- b964 - 成績指標.py
+++ b964 - 成績指標.py
@@ -1,33 +1,27 @@
-'''
-10
-0 11 22 33 55 66 77 99 88 44
-
-1
-13
-
-2
-73 65
-'''
-a = int(input())
-score = list(map(int, input().split()))
-score.sort()
-
-for i in score:
-    print(i, end = ' ')
-print()    
-
-for i in reversed(score):
-    if i < 60:
-        print(i)
-        break
-    if i == score[0] :
-        print('best case')
-
-for i in score:
-    if i >= 60:
-        print(i)
-        break
-    if i == score[-1] :
-        print('worst case')
-
+while True:
+    try:
+        a = int(input())
+        score = list(map(int, input().split()))
+        score.sort()
         
+        for i in score:
+            print(i, end = ' ')
+        print()    
+        
+        for i in reversed(score):
+            if i < 60:
+                print(i)
+                break
+            if i == score[0] :
+                print('best case')
+                break
+        
+        for i in score:
+            if i >= 60:
+                print(i)
+                break
+            if i == score[-1] :
+                print('worst case')
+                break
+    except Exception:
+        pass            
